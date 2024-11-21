c6c47c91033a90a0239f9e1391a29e385d93e75b
Hyunsoo Park <86937501+RamGI55@users.noreply.github.com>
2024-11-21 20:24:51 +0900

    Add files via upload

--- Classassign/Nov_21_bingo/main.cpp
+++ Classassign/Nov_21_bingo/main.cpp
@@ -3,7 +3,7 @@
 /// <summary>
 ///  SINGLEPLAYER - Tier 1 
 /// </summary>
-
+using namespace std;
 int verticalVerif;
 int gameOver; 
 
@@ -26,7 +26,7 @@ void Suffle(int(&bingoNumber[5][5]))
 {
 	srand(time(0));
 	int dest, sour, temp;
-	for (int a = 0; a < 200; a++)
+	for (int i = 0; i < 200; i++)
 	{
 
 		dest = rand() % 5;
@@ -53,39 +53,38 @@ void printScreen(int(&bingoNumber[5][5]))
 	}
 }
 
-void marker(const int(&bingoNumber[5][5]),int playerCallout)
+void marker(const int(&bingoNumber[5][5]),int Select)
 {
 	
 		for (int i = 0; i < 5; i++)
 		{
 			for (int j = 0; j < 5; j++)
 			{
-				if (bingoNumber[i][j] == playerCallout)
+				if (bingoNumber[i][j] == Select)
 
 					bingoNumber[i][j] = 0;
 			}
 
 		}
-		int count = 0;
+		//for (int i = 0; i < 5; i++)
+		//{
+		//	for (int j = 0; j < 5; j++)
+		//	{
+		//		if (bingoNumber[i][j] == 0) cout << (char)bingoNumber[i][j]; // TODO : Change to the #. 
+		//		else cout << bingoNumber[i][j] << "\t";
 
-		for (int i = 0; i < 5; i++)
-		{
-			for (int j = 0; j < 5; j++)
-			{
-				if (bingoNumber[i][j] == 36) cout << (char)bingoNumber[i][j]; // TODO : Change to the #. 
-				else cout << bingoNumber[i][j] << "\t";
+		//	}
+		//	cout << endl;
 
-			}
-			cout << endl;
-
-		}
+		//}
 	
 
 
 }
 
-void IsBingo (const int(&bingoNumber[5][5]), int bingocount)
-{
+int IsBingo(const int(&bingoNumber[5][5]), int bingocount)
+{	
+	int verticalVerif;
 	int horizontalVerif;
 	int rightDiagVerif;
 	int leftDiagVerif;
@@ -104,6 +103,28 @@ void IsBingo (const int(&bingoNumber[5][5]), int bingocount)
 
 			}
 			if (horizontalVerif == 5)
+			{
+
+				bingocount++;
+			}
+
+
+		}
+
+		for (int i = 0; i < 5; i++)
+		{
+			verticalVerif = 0;
+
+			for (int j = 0; j < 5; j++)
+			{
+				if (bingoNumber[j][i] == 0)
+				{
+					verticalVerif++;
+				}
+
+
+			}
+			if (verticalVerif == 5)
 			{
 
 				bingocount++;
@@ -133,7 +154,6 @@ void IsBingo (const int(&bingoNumber[5][5]), int bingocount)
 
 
 
-using namespace std; 
 int main()
 {	
 	
@@ -141,29 +161,26 @@ int main()
 	BfSuffle(player);
 	Suffle(player); 
 	int pBingoCount = 0;
-	
-
-
+	int pCallout; 
+	 
 	cout << "========= 5X5 BINGO GAME =========== " << endl;
-	
+	cout << pBingoCount << endl;
 	// TODO:: Make the loops work, check in the debug which illegal code stops to procced. 
-		for (int i = 0; i < 25 ; i ++)
+		for (int g = 0; g < 25 ; g ++)
 		{
 			printScreen(player);
-
-			if (pBingoCount >= 3)
+			IsBingo(player, pBingoCount);
+			
+		/*	if (pBingoCount == 3)
 			{
-				break;
+				
 			}
-
+			else*/ 
 			cout << "Please Select The Number: " << endl;
 			cin >> pCallout;
 
 			marker(player, pCallout);
-
-			IsBingo(player, pBingoCount);
-
 			
 		}
-		return 0; 
+		 
 }
